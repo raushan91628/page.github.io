06bfcc1fce6c2f549cafedf2f3c23ac693e9467e
raushan91628 <140476181+raushan91628@users.noreply.github.com>
2024-03-07 10:10:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,21 @@
             {
                 alert("Please fill all modetery fill");
                 return false;
-            }
+            } else if(isNaN(a))
+                {
+                    alert("Only digit are allow for phone number !Please Enter Only Number");
+                    return false;
+                } else if(a.length<10 || a.length>10)
+                {
+                    alert("Only 10 Digit Number are allow ! Please Enter Valid Number");
+                    return false;
+                }
             else if (a=="9708589431" && b=="kss@123")
                 {
                     alert("Login Successfully");
                   true ;
                 } else{
-                    alert("please Enter valid Phone number or Password ! Phone number should be 10 digit")
+                    alert("please Enter valid Phone number or Password ! Don't match your phone number or password")
                return false;
     
             }
